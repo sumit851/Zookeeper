277249040208f54a446694d8943b00910ce2d167
sumit851 <109820408+sumit851@users.noreply.github.com>
2023-07-30 00:14:33 +0530

    Create Zookeeper.js

---
+++ Zookeeper.js
@@ -0,0 +1,150 @@
+const input = require('readline-sync');
+
+const habitat  =[`
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \\
+     |     \\    _.-'             \\
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;     
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!`,
+
+lion = `
+Switching on the camera in the lion habitat...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; \`,       /\`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+,MM:.    .'.-'   .'     ;     \`\\    ;     \`,     MMMMMMMW \`"=./\`-,
+WMMm__,-'.'     /      _.\\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / \`=_}
+"^MP__.-'    ,-' _.--""   \`-,   ;       \\  ; ;MMMMMMMMMMW^\`\`; __|
+           /   .'            ; ;         )  )\`{  \\ \`"^W^\`,   \\  :
+          /  .'             /  (       .'  /     Ww._     \`.  \`"
+         /  Y,              \`,  \`-,=,_{   ;      MMMP\`""-,  \`-._.-,
+        (--, )                \`,_ / \`) \\/"")      ^"      \`-, -;"\\:
+The lion is roaring!`,
+
+deer = `
+Switching on the camera in the deer habitat...
+   /|       |\\
+\`__\\\\       //__'
+   ||      ||
+ \\__\`\\     |'__/
+   \`_\\\\   //_'
+   _.,:---;,._
+   \\_:     :_/
+     |@. .@|
+     |     |
+     ,\\.-./ \\
+     ;;\`-'   \`---__________-----.-.
+     ;;;                         \\_\\
+     ';;;                         |
+      ;    |                      ;
+       \\   \\     \\        |      /
+        \\_, \\    /        \\     |\\
+          |';|  |,,,,,,,,/ \\    \\ \\_
+          |  |  |           \\   /   |
+          \\  \\  |           |  / \\  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Our 'Bambi' looks hungry. Let's go to feed it!`,
+
+goose = `
+Switching on the camera in the goose habitat...
+
+                                    _
+                                ,-"" "".
+                              ,'  ____  \`.
+                            ,'  ,'    \`.  \`._
+   (\`.         _..--.._   ,'  ,'        \\    \\
+  (\`-.\\    .-""        ""'   /          (  d _b
+ (\`._  \`-"" ,._             (            \`-(   \\
+ <_  \`     (  <\`<            \\              \`-._\\
+  <\`-       (__< <           :
+   (__        (_<_<          ;
+    \`------------------------------------------
+The goose is staring intently at you... Maybe it's time to change the channel?`,
+
+bat = `
+Switching on the camera in the bat habitat...
+_________________               _________________
+ ~-.              \\  |\\___/|  /              .-~
+     ~-.           \\ / o o \\ /           .-~
+        >           \\\\  W  //           <
+       /             /~---~\\             \\
+      /_            |       |            _\\
+         ~-.        |       |        .-~
+            ;        \\     /        i
+           /___      /\\   /\\      ___\\
+                ~-. /  \\_/  \\ .-~
+                   V         V
+This bat looks like it’s doing fine.`,
+
+rabbit = `
+Switching on the camera in the rabbit habitat...
+         ,
+        /|      __
+       / |   ,-~ /
+      Y :|  //  /
+      | jj /( .^
+      >-"~"-v"
+     /       Y
+    jo  o    |
+   ( ~T~     j
+    >._-' _./
+   /   "~"  |
+  Y     _,  |
+ /| ;-"~ _  l
+/ l/ ,-"~    \\
+\\//\\/      .- \\
+ Y        /    Y
+ l       I     !
+ ]\\      _\\    /"\\
+(" ~----( ~   Y.  )
+It looks like we will soon have more rabbits!`]
+
+function main() {
+  let answer;
+  while (true) {
+    answer = input.question("Please enter the number of the habitat you would like to view (or 'exit' to end the program): ");
+  
+    // Check if the user wants to exit
+    if (answer.toLowerCase() === "exit") {
+      console.log("See you later!");
+      break;
+    }
+  
+    // Convert the user input to a number
+    const habitatNumber = Number(answer);
+  
+    // Check if the habitat number is valid
+    if (!isNaN(habitatNumber) && habitatNumber >= 0 && habitatNumber < habitat.length) {
+      console.log(habitat[habitatNumber]);
+    } else {
+      console.log("Invalid habitat number. Please enter a number between 0 and " + (habitat.length - 1) + ".");
+    }
+  }
+}
+
+module.exports = main;
+main();
+
+
